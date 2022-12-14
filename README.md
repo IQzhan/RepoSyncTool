d9e228bd898e7e8dc730cb9baac81763c0bf62c1
ZhanQI <zhanqi@onemt.com.cn>
2022-12-14 16:30:10 +0800

    first

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# sync_repo_tool
+# How to use
+Write into sync_repo.json like this:
+```json
+{
+    "repositories": [
+        {
+            "type": "git",
+            "url": "git url",
+            "path": "relative path",
+            "branch": "branch or tag"
+        },
+        {
+            "type": "svn",
+            "url": "svn url",
+            "path": "relative path",
+            "branch": "revision"
+        }
+    ]
+}
+```
+Then execute sync_repo.ps1
---
+++ sync_repo.ps1
@@ -0,0 +1,77 @@
+# Started directory
+$oriDir = Get-Location
+# Get the current directory
+$dir = Split-Path -Parent $MyInvocation.MyCommand.Path
+# Load the configuration
+$config = Get-Content "$dir/sync_repo.json" -Raw | ConvertFrom-Json
+# Iterate over the configured git repositories
+foreach ($repo in $config.repositories) {
+    # Target directory
+    $targetDir = Join-Path $dir $repo.path
+    # Git
+    if ($repo.type -eq "git") {
+        # check if the local path exists
+        if (Test-Path $targetDir) {
+            $removeOriginalGit = 'false'
+            $downloadNewGit = 'false'
+            # if the local path exists, check if it's a git repository and if it's the same as the remote
+            $gitPath = Join-Path $targetDir ".git"
+            if (Test-Path $gitPath) {
+                # if it's a git repository, check if it's the same as the remote
+                $currentRemoteURL = (git -C $targetDir remote get-url origin)
+                if ($currentRemoteURL -ne $repo.url) {
+                    # if the current remote is not the same as the desired remote
+                    $removeOriginalGit = 'true'
+                    $downloadNewGit = 'true'
+                }
+            }
+            else {
+                # if the local path is not a git repository
+                $downloadNewGit = 'true'
+            }
+            if ($removeOriginalGit -eq 'true') {
+                # Remove '.git' folder
+                Remove-Item -Path $gitPath -Recurse -Force
+            }
+            if ($downloadNewGit -eq 'true') {
+                # Will create 'temp_git' as temp folder for matiain '.git' folder whitch clone from remote
+                $tempPath = Join-Path $targetDir "temp_git"
+                # Clone from remote to 'temp_git', '--no-checkout' makes sure only '.git' folder will be create
+                git clone --no-checkout $repo.url $tempPath
+                # Move this new '.git' folder to $targetDir
+                $tempPathGit = Join-Path $tempPath ".git"
+                Move-Item -Path $tempPathGit -Destination $targetDir
+                # Remove 'temp_git' folder
+                Remove-Item -Path $tempPath -Recurse -Force
+            }
+        }
+        else {
+            # if the local path does not exist
+            # Checkout the repository
+            git clone $repo.url $targetDir
+        }
+        # Switch to the repository directory
+        Set-Location $targetDir
+        # Checkout the specified branch or tag
+        git checkout $repo.branch --force
+        # If in branch, force update to the latest version
+        git pull --force
+    }
+    # Svn
+    elseif ($repo.type -eq "svn") {
+        # Check out the repository from the specified URL and branch or tag
+        svn checkout --force $repo.url --revision $repo.branch $targetDir
+        # Switch to the repository directory
+        Set-Location $targetDir
+        # Force update to the latest version
+        svn update --force --accept=theirs-full
+        # Revert any changes if there is no update
+        svn revert --depth infinity $targetDir
+    }
+    else {
+        Write-Output "'type' must be 'git' or 'svn'."
+    }
+}
+Write-Output "Update repositories finished."
+# Back to started directory
+Set-Location $oriDir
